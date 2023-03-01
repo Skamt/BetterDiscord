f0edc49a45a1b7a2ab42124f3ec57a58d7047b0f
Skamt <114232893+Skamt@users.noreply.github.com>
2023-03-01 23:20:13 +0100

    no message

--- renderer/src/ui/settings/sidebarheader.jsx
+++ renderer/src/ui/settings/sidebarheader.jsx
@@ -3,12 +3,7 @@ import {DiscordModules, React} from "modules";
 import HistoryIcon from "../icons/history";
 import Modals from "../modals";
 
-<<<<<<< HEAD
-const Tooltip = WebpackModules.getByPrototypes("renderTooltip");
-if(!SidebarComponents) debugger;
-=======
 
->>>>>>> d1183633c0e4411c929cb4f9ec6f9745d8a7ee32
 export default class SettingsTitle extends React.Component {
     renderHeader() {
         return <h2 className="bd-sidebar-header-label">BetterDiscord</h2>;
