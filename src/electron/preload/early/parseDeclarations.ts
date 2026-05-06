233ae270740720061e39d37881cf77083375feb8
TheLazySquid <76746384+TheLazySquid@users.noreply.github.com>
2026-05-06 17:03:40 -0400

    Replace acorn with custom minimal parser

---
+++ src/electron/preload/early/parseDeclarations.ts
@@ -0,0 +1,274 @@
+const enum CharCodes {
+    OpenBrace = 123,
+    CloseBrace = 125,
+    OpenParenthesis = 40,
+    CloseParenthesis = 41,
+    OpenBracket = 91,
+    CloseBracket = 93,
+    DoubleQuote = 34,
+    SingleQuote = 39,
+    Backtick = 96,
+    Slash = 47,
+    Backslash = 92,
+    Semicolon = 59,
+    Colon = 58,
+    Equals = 61,
+    Comma = 44,
+    Zero = 48,
+    Nine = 57,
+    CaptialA = 65,
+    CaptialZ = 90,
+    LowerA = 97,
+    LowerZ = 122,
+    Underscore = 95,
+    Dollar = 36,
+    Space = 32,
+    Exclamation = 33
+}
+
+const singleDeclarations = ["function ", "async function ", "class "];
+const multiDeclarations = ["let", "var", "const"];
+
+// This is very specifically for Discord's minified code
+export default function parseDeclarations(moduleString: string, baseDepth = 1) {
+    let depth = 0;
+    let inDeclaration = false;
+    let isDestructuring = false;
+    let declarations: string[] = [];
+
+    for (let i = 0; i < moduleString.length; i++) {
+        const code = moduleString.charCodeAt(i);
+        checkChar: switch (code) {
+            case CharCodes.OpenBrace:
+            case CharCodes.OpenParenthesis:
+            case CharCodes.OpenBracket:
+                depth++;
+                break;
+            case CharCodes.CloseBrace:
+            case CharCodes.CloseParenthesis:
+            case CharCodes.CloseBracket:
+                depth--;
+                break;
+            case CharCodes.DoubleQuote:
+            case CharCodes.SingleQuote:
+                i = findEndOfString(moduleString, i + 1, code);
+                break;
+            case CharCodes.Backtick:
+                i = findEndOfTemplateLiteral(moduleString, i + 1);
+                break;
+            case CharCodes.Slash:
+                // Ignore regex
+                const lastCode = moduleString.charCodeAt(i - 1);
+                if (
+                    (!isVariableCharacter(lastCode) || moduleString.startsWith("return", i - 6)) &&
+                    lastCode !== CharCodes.CloseParenthesis &&
+                    lastCode !== CharCodes.CloseBracket
+                ) {
+                    i = findEndOfRegex(moduleString, i + 1);
+                }
+                break;
+            default:
+                if (isDestructuring) {
+                    // Check if we are no longer destructuring
+                    if (depth <= baseDepth) {
+                        isDestructuring = false;
+                        if (depth !== baseDepth) break;
+                    } else if (depth !== baseDepth + 1) {
+                        break;
+                    }
+                } else if (depth !== baseDepth) {
+                    break;
+                }
+
+                // If we find a declaration keyword begin the declaration state
+                if (!inDeclaration) {
+                    // Check if there is a single declaration (function/class)
+                    for (const keyword of singleDeclarations) {
+                        if (!moduleString.startsWith(keyword, i)) continue;
+
+                        const charBefore = moduleString.charCodeAt(i - 1);
+                        if (charBefore === CharCodes.Equals || charBefore === CharCodes.Exclamation) continue;
+
+                        const [declaration] = getVariableName(moduleString, i + keyword.length);
+                        declarations.push(declaration);
+                        i += declaration.length + keyword.length - 1;
+
+                        break checkChar;
+                    }
+
+                    for (const keyword of multiDeclarations) {
+                        if (!moduleString.startsWith(keyword, i)) continue;
+
+                        // If the next character isn't whitespace or destructuring skip it
+                        const nextChar = moduleString.charCodeAt(i + keyword.length);
+                        if (nextChar === CharCodes.Space) i++;
+                        else if (nextChar !== CharCodes.OpenBrace && nextChar !== CharCodes.OpenBracket) continue;
+
+                        const [declaration, nowDestructuring] = getVariableName(moduleString, i + keyword.length);
+
+                        // If we're destructuring, increase depth
+                        if (nowDestructuring) {
+                            isDestructuring = true;
+                            depth++;
+                        }
+
+                        // Add the declaration
+                        declarations.push(declaration);
+                        i += keyword.length + declaration.length - 1;
+                        inDeclaration = true;
+
+                        break checkChar;
+                    }
+
+                    break;
+                }
+
+                // If we hit a semicolon the declaration is done
+                if (code === CharCodes.Semicolon) {
+                    inDeclaration = false;
+                    break;
+                }
+
+                // If we're at a comma, add the next variable
+                if (code === CharCodes.Comma) {
+                    const [declaration, nowDestructuring] = getVariableName(moduleString, i + 1);
+
+                    if (nowDestructuring) {
+                        isDestructuring = true;
+                        depth++;
+                    }
+
+                    declarations.push(declaration);
+                    i += declaration.length;
+                }
+        }
+    }
+
+    return declarations;
+}
+
+function getVariableName(moduleString: string, startIndex: number): [string, boolean] {
+    const firstCode = moduleString.charCodeAt(startIndex);
+    const isDestructuring = firstCode === CharCodes.OpenBrace || firstCode === CharCodes.OpenBracket;
+    if (isDestructuring) startIndex++;
+
+    for (let i = startIndex + 1; i < moduleString.length; i++) {
+        const code = moduleString.charCodeAt(i);
+
+        // If we're at a colon the variable is being renamed in an object destructure
+        if (code === CharCodes.Colon) {
+            startIndex = i + 1;
+        } else if (!isVariableCharacter(code)) {
+            return [moduleString.slice(startIndex, i), isDestructuring];
+        }
+    }
+
+    return [moduleString.slice(startIndex), isDestructuring];
+}
+
+function findEndOfString(moduleString: string, startIndex: number, quoteChar: CharCodes) {
+    let shouldEscape = false;
+
+    // Go until we find a non-escaped quote character
+    for (let i = startIndex; i < moduleString.length; i++) {
+        const code = moduleString.charCodeAt(i);
+
+        if (code === quoteChar && !shouldEscape) return i;
+
+        // Escape characters if there are an odd number of backslashes before them
+        if (code === CharCodes.Backslash) shouldEscape = !shouldEscape;
+        else shouldEscape = false;
+    }
+
+    return moduleString.length;
+}
+
+function findEndOfRegex(moduleString: string, startIndex: number) {
+    let inCharacterSet = false;
+    let shouldEscape = false;
+
+    for (let i = startIndex; i < moduleString.length; i++) {
+        const code = moduleString.charCodeAt(i);
+
+        if (inCharacterSet) {
+            if (code === CharCodes.CloseBracket && !shouldEscape) {
+                inCharacterSet = false;
+            }
+        } else {
+            if (code === CharCodes.Slash && !shouldEscape) {
+                return i;
+            } else if (code === CharCodes.OpenBracket && !shouldEscape) {
+                inCharacterSet = true;
+            }
+        }
+
+        if (code === CharCodes.Backslash) shouldEscape = !shouldEscape;
+        else shouldEscape = false;
+    }
+
+    return moduleString.length;
+}
+
+// This does a little recursion (bad), but nested template literals are very rare so it's fine
+function findEndOfTemplateLiteral(moduleString: string, startIndex: number) {
+    let depth = 0;
+    let lastCode = 0;
+    let lastLastCode = 0;
+
+    for (let i = startIndex; i < moduleString.length; i++) {
+        const code = moduleString.charCodeAt(i);
+
+        if (depth === 0) {
+            // If we found the end, stop
+            if (code === CharCodes.Backtick && lastCode !== CharCodes.Backslash) {
+                return i;
+            }
+
+            // If we're at a ${, increase the depth and try to find the end
+            if (code === CharCodes.OpenBrace && lastCode === CharCodes.Dollar && lastLastCode !== CharCodes.Backslash) {
+                depth++;
+            }
+        } else {
+            switch (code) {
+                case CharCodes.OpenBrace:
+                    depth++;
+                    break;
+                case CharCodes.CloseBrace:
+                    depth--;
+                    break;
+                case CharCodes.DoubleQuote:
+                case CharCodes.SingleQuote:
+                    i = findEndOfString(moduleString, i + 1, code);
+                    break;
+                case CharCodes.Backtick:
+                    i = findEndOfTemplateLiteral(moduleString, i + 1);
+                    break;
+                case CharCodes.Slash:
+                    const lastCode = moduleString.charCodeAt(i - 1);
+                    if (
+                    (!isVariableCharacter(lastCode) || moduleString.startsWith("return", i - 6)) &&
+                        lastCode !== CharCodes.CloseParenthesis &&
+                        lastCode !== CharCodes.CloseBracket
+                    ) {
+                        i = findEndOfRegex(moduleString, i + 1);
+                    }
+                    break;
+            }
+        }
+
+        lastLastCode = lastCode;
+        lastCode = code;
+    }
+
+    return moduleString.length;
+}
+
+function isVariableCharacter(code: number) {
+    return (
+        (code >= CharCodes.LowerA && code <= CharCodes.LowerZ) ||
+        (code >= CharCodes.CaptialA && code <= CharCodes.CaptialZ) ||
+        (code >= CharCodes.Zero && code <= CharCodes.Nine) ||
+        code === CharCodes.Underscore ||
+        code === CharCodes.Dollar
+    );
+}
---
+++ tests/early/parseDeclarations.test.ts
@@ -0,0 +1,64 @@
+import {describe, test, expect} from "bun:test";
+import parseDeclarations from "../../src/electron/preload/early/parseDeclarations";
+
+describe("parseDeclarations", () => {
+    test("function wrapper", () => {
+        const tested = `function(){let a=1;var b=2;const c=3;}`;
+        expect(parseDeclarations(tested)).toEqual(["a", "b", "c"]);
+    });
+
+    test("commas", () => {
+        const tested = `let a=1,b=2;const c=3,d,e=15`;
+        expect(parseDeclarations(tested, 0)).toEqual(["a", "b", "c", "d", "e"]);
+    });
+
+    test("functions and classes", () => {
+        const tested = `function a(){let a=1;var b=2;const c=3;}async function b(){}class c{}class d extends z{};!function w(){}`;
+        expect(parseDeclarations(tested, 0)).toEqual(["a", "b", "c", "d"]);
+    });
+
+    test("complex values", () => {
+        const tested = `let a={x:1,y:2},b=[1,2,3],c="{\\"}",d='string\\\\',e=\`str\${"string"}ing\`,f=(()=>{})().test({}),g=15`;
+        expect(parseDeclarations(tested, 0)).toEqual(["a", "b", "c", "d", "e", "f", "g"]);
+    });
+
+    test("array destructuring", () => {
+        const tested = `let[a,b,c,d]=[],[e=15,f={},g=()=>{}]=[];`;
+        expect(parseDeclarations(tested, 0)).toEqual(["a", "b", "c", "d", "e", "f", "g"]);
+    });
+
+    test("object destructuring", () => {
+        const tested = `const{a,q:b,c,d}={},e=15,{f={},uh:g=()=>{}}={};`;
+        expect(parseDeclarations(tested, 0)).toEqual(["a", "b", "c", "d", "e", "f", "g"]);
+    });
+
+    test("regex", () => {
+        const tested = `10/5;(10)/5;[10][0]/5;let a=/\\/[^/\\\\]$/,b=/\\(\\)/;`;
+        expect(parseDeclarations(tested, 0)).toEqual(["a", "b"]);
+    });
+
+    test("single variables", () => {
+        const tested = `var a;a=function(){},"function"==typeof b`;
+        expect(parseDeclarations(tested, 0)).toEqual(["a"]);
+    });
+
+    test("nested template literals", () => {
+        const tested = "let a=`${`, ${``}`})`,b=15";
+        expect(parseDeclarations(tested, 0)).toEqual(["a", "b"]);
+    });
+
+    test("ignore named functions as values", () => {
+        const tested = `let a,b=15;e.exports=function z(){}`;
+        expect(parseDeclarations(tested, 0)).toEqual(["a", "b"]);
+    });
+
+    test("variables in properties", () => {
+        const tested = `a.variable=15;const b=15;`;
+        expect(parseDeclarations(tested, 0)).toEqual(["b"]);
+    });
+
+    test("regex after return", () => {
+        const tested = `function a(){return/}/}function b(){}`;
+        expect(parseDeclarations(tested, 0)).toEqual(["a", "b"]);
+    });
+});
